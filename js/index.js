2d7752a994af1d3736e32af851758d6d17d2baa4
Giovana Rocha <100931504+giovanarochaz@users.noreply.github.com>
2023-03-07 18:22:48 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,19 @@
+function index(){
+    window.location.href = "index.html"
+}
+
+function exemplo(){
+    window.location.href = "exemplo.html"
+}
+
+function formuario(){
+    window.location.href = "forms.html"
+}
+
+function revolucao(){
+    window.location.href = "revolucoes.html"
+}
+
+function impacto(){
+    window.location.href = "impactos.html"
+}
